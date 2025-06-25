63d996bc6d9f576da04e141f552894067a02a5de
Abby Shen <143675084+aeshen27@users.noreply.github.com>
2025-06-24 20:57:23 -0500

    initial database table rendering

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
 from flask import Blueprint, render_template
+import mysql.connector
+import config
 
 main = Blueprint('main', __name__)
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user=config.DB_USER,
+    password=config.DB_PASSWORD,
+    database=config.DB_NAME
+)
 
 @main.route('/')
 def home():
     return "<h1>Hello from Flask!</h1>"
+
+@main.route('/companies')
+def view_companies():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM companies")
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('companies.html', companies = data)
