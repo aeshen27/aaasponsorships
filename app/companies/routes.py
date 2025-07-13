87561c2cf63b986c0139eca1644b2addde16305e
Abby Shen <143675084+aeshen27@users.noreply.github.com>
2025-07-13 16:04:30 -0500

    modal

--- app/companies/routes.py
+++ app/companies/routes.py
@@ -11,3 +11,17 @@ def list_companies():
     data = cursor.fetchall()
     cursor.close()
     return render_template('companies/list.html', companies = data)
+
+@companies.route('/add', methods=['GET', 'POST'])
+def add_company():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        
+        conn = db.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("INSERT ()")
+        
+        return redirect(url_for('companies.list_companies'))
+    
+    return render_template('companies/add.html')
